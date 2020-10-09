8585d31a445ce1ec7828cb5dc6558d842a57f121
Prakash Vadrevu <prakash.vadrevu@hotmail.com>
2020-10-09 12:39:12 +0530

    Characterization test

--- src/test/java/com/movierental/CustomerTest.java
+++ src/test/java/com/movierental/CustomerTest.java
@@ -1,9 +1,25 @@
 package com.movierental;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CustomerTest {
+
     @Test
-    public void test(){}
+    public void shouldTestStatement(){
+        Customer customer = new Customer("Prakash");
+        customer.addRental(new Rental(new Movie("Jhansi the Timekeepr", Movie.CHILDRENS), 4));
+        customer.addRental(new Rental(new Movie("Movie 2", Movie.REGULAR), 7));
+        customer.addRental(new Rental(new Movie("Movie 3", Movie.NEW_RELEASE), 2));
+
+        String statement = customer.statement();
+
+        Assert.assertEquals(statement, "Rental Record for Prakash\n" +
+                "\tJhansi the Timekeeper\t3.0\n" +
+                "\tMovie 2\t9.5\n" +
+                "\tMovie 3\t6.0\n" +
+                "Amount owed is 18.5\n" +
+                "You earned 4 frequent renter points");
+    }
 
 }
